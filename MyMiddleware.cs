f7719ccf3dcc854a4e185165b3d9b3e88a7fd2b9
Wesley Alves <wesleyalves@WesleyOS.local>
2018-03-11 00:55:16 -0300

    Middleware basic ResponseTime

--- MyMiddleware.cs
+++ MyMiddleware.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 
@@ -13,11 +14,13 @@ public class MyMiddleware
     public async Task Invoke(HttpContext context)
     {
         //Request
-
+        var start = DateTime.Now;
 
         await _next(context);
-        
 
+        var finish = DateTime.Now;
+        var diff = finish.Subtract(start);
+        await context.Response.WriteAsync($"The time was {diff.Milliseconds}");
         //Response
     }
 }
